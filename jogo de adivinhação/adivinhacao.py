56fab0c1193e57a43becd7786ae54a4aba8f61ef
joaofranciscodossantosneto <126591059+joaofranciscodossantosneto@users.noreply.github.com>
2025-02-25 10:52:30 -0300

    Add files via upload

--- jogo de adivinhação/adivinhacao.py
+++ jogo de adivinhação/adivinhacao.py
@@ -2,23 +2,29 @@ print('*****************')
 print('Bem vindo ao jogo')
 print('*****************')
 
-chute = int(input('Digite o seu número: '))
+numero_secreto=12
+total_de_tentativas = 3
+rodada=1
+while  ( rodada <= total_de_tentativas ):
+    print(f"Tentativa {rodada} de {total_de_tentativas} ")
+    chute = int(input('Digite o seu número: '))
 
-numero_secreto = 12
-acertou = numero_secreto == chute
-chute_maior = chute > numero_secreto
-chute_menor = chute < numero_secreto
+    acertou = numero_secreto == chute
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
 
 
-print(f'Você digitou {chute}') 
+    print(f'Você digitou {chute}') 
 
-if(numero_secreto == chute):
-    print('Você acertou!')
-else:
-    if(chute > numero_secreto):
-        print('Você errou! Seu número foi maior do que o número secreto')
-    elif(chute < numero_secreto):
-        print('Você errou! Seu número foi menor do que o número secreto')
+    if(numero_secreto == chute):
+        print('Você acertou!')
+    else:
+        if(chute > numero_secreto):
+            print('Você errou! Seu número foi maior do que o número secreto')
+        elif(chute < numero_secreto):
+            print('Você errou! Seu número foi menor do que o número secreto')
+
+    rodada = rodada + 1
 
     print('@@@@@@@@@@@')
     print('Fim de jogo')
